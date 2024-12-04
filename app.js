965fdfcebf9787eaf64d70ea924e1da64ba081ce
sahilmund.6685@gmail.com <sahilmund.6685@gmail.com>
2024-12-05 00:36:18 +0530

    server created and ejs added setup completed

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const http = require('http');
+
+const socketIo = require('socket.io');
+const server = http.createServer(app);
+const io = socketIo(server);
+
+
+
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req,res) => {
+    res.send("Hello World");
+
+})
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+})
+
